5aff3fbb89cdab79784c6ec7a570c3d382a6df0c
Diego Torres <diegotc86@gmail.com>
2024-04-26 12:34:56 -0500

    add json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import pokemons from '../data/pokemons.json';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-components';
+  pokemons = pokemons;
+
+  ngOnInit() {
+    console.log(pokemons);
+  }
 }
