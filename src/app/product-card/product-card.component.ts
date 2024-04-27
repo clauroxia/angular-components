c586d0ce0c7adb5e9f98cd0d8d5bdd69011426b4
JaimeEnriquezVivanco <jaime.enriquez.vivanco@gmail.com>
2024-04-27 00:16:58 -0500

    feat: product-list passes state to store component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductQuantity } from '../types/productQuantity';
 @Component({
   selector: 'app-product-card',
   standalone: true,
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
+  @Input() product_id = 2
+  @Input() product_name = 'n'
+  @Input() product_category = 'cat'
+  @Input() product_price = 2.5
+  @Input() product_img_url = 'https://res.cloudinary.com/dwdgpw20b/image/upload/v1698330380/products/milk_vmhcfw.webp'
 
+  quantity = 0
+  @Output() quantityChange = new EventEmitter<ProductQuantity>()
+
+  increaseQuantity(){
+    this.quantity += 1
+    this.quantityChange.emit({
+      id: this.product_id,
+      quantity: this.quantity
+    })
+  }
+
+  decreaseQuantity(){
+    if (this.quantity === 0) return
+    this.quantity -= 1
+    this.quantityChange.emit({
+      id: this.product_id,
+      quantity: this.quantity
+    })
+  }
 }
--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
-
+import { Order } from '../types/Order';
 @Component({
   selector: 'app-store',
   standalone: true,
@@ -9,4 +9,11 @@ import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component'
   templateUrl: './store.component.html',
   styleUrl: './store.component.css',
 })
-export class StoreComponent { }
+export class StoreComponent {
+  order: Order = []
+
+  onOrderChange(newOrder: Order){
+    this.order = newOrder
+    console.log(this.order)
+  }
+}
