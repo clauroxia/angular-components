32dff6d2f2ae8877795fc9fdc302692ecc5a46d1
JaimeEnriquezVivanco <jaime.enriquez.vivanco@gmail.com>
2024-04-27 18:14:51 -0500

    feat: implement most of shopping-cart  functionality
    todo:
    - style according to figma
    - view when shopping-cart is empty
    - refactor to avoid unnecessary db queries (product-card should provide more information)

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Order } from '../types/Order';
+import { ProductQuantity } from '../types/productQuantity';
+import products from '../../data/products.json'
 
 @Component({
   selector: 'app-shopping-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shopping-cart.component.html',
   styleUrl: './shopping-cart.component.css'
 })
 export class ShoppingCartComponent {
+  @Input() order: Order = []
+  products = products
 
+  calculateTotal(){
+    let  total = 0
+    for (let product of this.order){
+      let quantity = product.quantity
+      let price = products[product.id - 1].price
+      total += quantity * price
+    }
+
+    return total
+  }
+
+  trackByProductId(_index: number, productQuantity: ProductQuantity){
+    return productQuantity.id
+  }
 }
