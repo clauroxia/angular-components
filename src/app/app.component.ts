3a2c3aa6c3306ae6ea968663a54b66e12095e5f5
Hypatia Apaestegui <patyapaesteguicorp@gmail.com>
2024-04-26 23:38:40 -0500

    Add: Counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CounterComponent } from './components/counter/counter.component';
 import pokemons from '../data/pokemons.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
